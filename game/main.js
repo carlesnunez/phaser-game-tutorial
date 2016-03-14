c3beddb25b8a87be4c222962a33d8c6316753e18
Carles Nuñez <carles.nunez@zitrointeractive.com>
2016-03-14 10:34:37 +0100

    (FEAT)Added enemy generator, enemy object and closed the game flow. LAST ITERATION

--- game/main.js
+++ game/main.js
@@ -8,9 +8,9 @@ window.onload = function () {
 
   // Game States
   game.state.add('boot', require('./states/boot'));
-  //game.state.add('gameover', require('./states/gameover'));
+  game.state.add('gameover', require('./states/gameover'));
   game.state.add('menu', require('./states/menu'));
-  //game.state.add('play', require('./states/play'));
+  game.state.add('play', require('./states/play'));
   game.state.add('preload', require('./states/preload'));
 
 
